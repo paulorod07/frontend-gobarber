caf46eaffa021cedf147e27e64d225a6bf30bb65
Paulo Rodrigues <pauloadesr@hotmail.com>
2020-08-26 15:44:40 -0300

    header dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   return (
     <Container>
-      <h1>hello Dashboard</h1>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 };
